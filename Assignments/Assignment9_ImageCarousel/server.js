1460f4306dadf9076089f8069bb84f6c701980ea
shashank <subedishashank2@gmail.com>
2022-06-14 10:04:43 +0545

    express assignment9

---
+++ Assignments/Assignment9_ImageCarousel/server.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+app.set('view engine', 'ejs')
+app.use(express.static('js'))
+app.use(express.static('css'))
+app.use(express.static('images'))
+app.get("/",(req,res)=>{
+    res.render('index')
+})
+app.listen(3000)
