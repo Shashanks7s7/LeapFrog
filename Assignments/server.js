6f812ad2b7af06d8b52777ec70a06402ea51f934
Shashanks7s7 <52647286+Shashanks7s7@users.noreply.github.com>
2022-06-19 00:45:00 +0545

    wrong file

--- Assignments/server.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const app=express()
-app.set('view engine', 'ejs')
-app.use(express.static('js'))
-app.use(express.static('Style'))
-app.get("/",(req,res)=>{
-    res.render('newsblog')
-})
-app.listen(3000) 
